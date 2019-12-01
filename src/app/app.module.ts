8126291f1ad4a69175561210f4114575cbc3bde0
Matt Lisiak <bocoroth@gmail.com>
2019-12-01 10:30:23 -0500

    fa icons, settings component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { TooltipModule } from 'ngx-bootstrap/tooltip'
 import { AppRoutingModule } from './app-routing.module'
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
 import { HttpClientModule } from '@angular/common/http'
+import { AngularFontAwesomeModule } from 'angular-font-awesome'
 import { AngularEditorModule } from '@kolkov/angular-editor'
 
 import { AppComponent } from './app.component'
@@ -12,6 +13,7 @@ import { EditorComponent } from './view/editor/editor.component'
 import { RehearsalComponent } from './view/rehearsal/rehearsal.component'
 import { PerformanceComponent } from './view/performance/performance.component'
 import { EditBoxComponent } from './panel/edit-box/edit-box.component'
+import { SettingsComponent } from './view/settings/settings.component'
 
 @NgModule({
   declarations: [
@@ -20,9 +22,18 @@ import { EditBoxComponent } from './panel/edit-box/edit-box.component'
     EditorComponent,
     RehearsalComponent,
     PerformanceComponent,
-    EditBoxComponent
+    EditBoxComponent,
+    SettingsComponent
   ],
-  imports: [BrowserModule, AppRoutingModule, TooltipModule.forRoot(), NgbModule, HttpClientModule, AngularEditorModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    TooltipModule.forRoot(),
+    NgbModule,
+    HttpClientModule,
+    AngularFontAwesomeModule,
+    AngularEditorModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
